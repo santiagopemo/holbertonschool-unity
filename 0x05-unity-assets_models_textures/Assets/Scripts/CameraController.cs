0ea0f687c5384d170c50eac5ad6a2d5cb6cb609f
santiagopemo <santiagopemo@gmail.com>
2021-01-30 20:36:11 -0500

    added tasks from 0 to 7

--- 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -6,23 +6,21 @@ public class CameraController : MonoBehaviour
 {
     private Vector3 offset;
     private GameObject player;
-    public float rotationSpeed = 10.0f;
+    public float turnSpeed = 4.0f;
 
-    float xRotation = 0;
-    float yRotation = 0;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-        offset = transform.position - player.transform.position; 
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        yRotation -= Input.GetAxis("Mouse Y") * rotationSpeed;
-        xRotation += Input.GetAxis("Mouse X") * rotationSpeed;
-        transform.eulerAngles = new Vector3(yRotation, xRotation, 0) ;
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.left) * offset;
+        // offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
         transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
     }
 }
