ee029cfc2b9b96722bf12bd11ebc5ee012997aad
santiagopemo <santiagopemo@gmail.com>
2021-01-30 23:00:47 -0500

    added tasks from 8 to 9

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+         player = FindObjectOfType<PlayerController>().gameObject;
+         timerText = GameObject.Find("TimerCanvas").transform.Find("TimerText").GetComponent<Text>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<Timer>().enabled = false;
+        timerText.color = Color.green;
+        timerText.fontSize = 60;
+    }
+}
