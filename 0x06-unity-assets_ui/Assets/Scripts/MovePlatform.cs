aa9f52f72e53bf3368fe8c40dd80030db8c3a208
santiagopemo <santiagopemo@gmail.com>
2021-02-17 21:00:49 -0500

    updated task 0

--- 0x06-unity-assets_ui/Assets/Scripts/MovePlatform.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/MovePlatform.cs
@@ -8,29 +8,54 @@ public class MovePlatform : MonoBehaviour
     public Vector3 targetPosition;
     public Vector3 initialPosition;
 
+    public float waitSeconds = 0;
+    public float waitStart = 0;
+
+    public bool startTrigger = false;
+
     public bool forward = true;
 
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = transform.position;
+        StartCoroutine(WaitStart(waitStart));
+    }
+    IEnumerator WaitStart(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        startTrigger = true;
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        forward = !forward;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (startTrigger == false)
+            return;
+
         float step = speed * Time.deltaTime;
-        if (forward)
+        
+        if (forward && Vector3.Distance(transform.position, targetPosition) != 0f)
         {
             transform.position = Vector3.MoveTowards(transform.position,  targetPosition, step);
             if (Vector3.Distance(transform.position, targetPosition) == 0f)
-                forward = false;
+            {
+                StartCoroutine(Wait(waitSeconds));               
+            }
         }
-        else
+        else if (forward == false && Vector3.Distance(transform.position, initialPosition) != 0f)
         {
             transform.position = Vector3.MoveTowards(transform.position,  initialPosition, step);
             if (Vector3.Distance(transform.position, initialPosition) == 0f)
-                forward = true;
+            {
+                StartCoroutine(Wait(waitSeconds));
+            }                
         }
     }
 }
